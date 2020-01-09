df7d5bd9a6e696ab1b62af147718cb330f381b09
Frans Dekkers <fde.gma@gmail.com>
2020-01-09 22:25:02 +0100

    Added FamilyTree

---
+++ src/app/FamilyTree.ts
@@ -0,0 +1,15 @@
+export class FamilyTree {
+    RecordId: number;
+    PersonsFather: number;
+    FatherFetched: boolean;
+    PersonsMother: number;
+    MotherFetched: boolean;
+    PersonsParentsArePartners: boolean;
+    PersonsPartner: number;
+    PartnerFetched: boolean;
+    PersonId: number;
+    PersonName: string;
+    PersonBirth: Date;
+    PersonDeath: Date;
+    PersonIsMale: boolean;
+}
