7deeb8463b4bb6ab78edc445aaf84e104524a30d
Frans Dekkers <fde.gma@gmail.com>
2020-04-20 20:06:54 +0200

    Progressing with state management

--- src/app/statemanagement.service.ts
+++ src/app/statemanagement.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { DatePipe } from '@angular/common';
+import { Subscription } from 'rxjs/Subscription';
 
 
 @Injectable({
@@ -8,23 +9,62 @@ import { DatePipe } from '@angular/common';
 })
 
 export class StateManagementService {
+    private plainpersonlist: {};
+    private IntermPers: any;
+    private namesToLookFor: string;
+    private indexOfPerson: number;
+    private possibleFathersList = {};
+    private possibleMothersList = {};
+    private possiblePartnersList = {};
+    private formContainsValue = false;
+    private formCanBeSubmitted = false;
+    private actionAtHand: string;
+    private initial = true;
+    message: any;
+    incomingMessage: Subscription;
+    theMessageObject: object;
 
-  private varOne: any;
-  private varTwo: string;
+    constructor(private datepipe: DatePipe) {}
 
-  constructor(private datepipe: DatePipe) {}
+    private handleError<T> (operation = 'operation', results?: T) {
+        return (error: any): Observable<T> => {
 
-  private handleError<T> (operation = 'operation', results?: T) {
-    return (error: any): Observable<T> => {
+        // TODO: send the error to default logging service
+        console.error(error);
 
-      // TODO: send the error to default logging service
-      console.error(error);
+        // TODO: transform error to make it user readeable
+        // this.log('${operation' failed: ${error.message}');
 
-      // TODO: transform error to make it user readeable
-      // this.log('${operation' failed: ${error.message}');
+        // Return empty result so the app keeps running
+        return of(results as T);
+        };
+    }
 
-      // Return empty result so the app keeps running
-      return of(results as T);
-    };
-  }
+    SetStatusBeforeLeavingPersonScreen( plainpersonlistIn: {},
+                            IntermPersIn: any,
+                            namesToLookForIn: string,
+                            indexOfPersonIn: number,
+                            possibleFathersListIn: {},
+                            possibleMothersListIn: {},
+                            possiblePartnersListIn: {},
+                            formContainsValueIn: boolean,
+                            formCanBeSubmittedIn: boolean,
+                            actionAtHandIn: string) {
+        this.plainpersonlist = plainpersonlistIn,
+        this.IntermPers = IntermPersIn,
+        this.namesToLookFor =  namesToLookForIn,
+        this.indexOfPerson = indexOfPersonIn,
+        this.possibleFathersList = possibleFathersListIn,
+        this.possibleMothersList = possibleMothersListIn,
+        this.possiblePartnersList = possiblePartnersListIn,
+        this.formContainsValue = formContainsValueIn;
+        this.formCanBeSubmitted = formCanBeSubmittedIn;
+        this.actionAtHand = actionAtHandIn;
+        this.initial = false;
+    }
+
+    GetStatusAfterReturningToPerson() {
+
+    }
+
 }
